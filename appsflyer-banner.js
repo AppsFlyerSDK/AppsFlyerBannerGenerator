aaf473057438284fb4eb2e0f5452e202381316b2
Dror Davidi <dror@appsflyer.com>
2018-02-14 19:14:59 +0200

    Adding AppsFlyer Banner Generator open source

---
+++ appsflyer-banner.js
@@ -0,0 +1,110 @@
+/**
+ * AppsFlyer Banner Generator
+ * Feb-2018, MIT License
+ */
+
+var CALL_TO_ACTION_TEXT = "Install";
+
+function AFBanner () {
+
+    this.buildUrl = function(settings) {
+        
+        function getParam(label, value) {
+            return value ? "&" + label + "=" + encodeURIComponent(value) : "";
+        }
+
+        // URL settings
+        var subdomain = settings.subdomain;
+        var onelinkid = settings.onelink_id;
+        var bannerTag = "?is_banner=true";
+        var baseUrl = "https://" + subdomain + ".onelink.me/" + onelinkid + bannerTag;
+        
+        // Attribution Settings
+        var media_source = getParam("pid", settings.media_source);
+        var campaign = getParam("c", settings.campaign);       
+        var ad = getParam("af_ad", settings.ad);
+        var ad_id = getParam("af_ad_id", settings.ad_id);
+        var site_id = getParam("af_siteid", settings.site_id);
+        var sub1 = getParam("af_sub1", settings.sub1);
+
+        // Deep link Settings
+        var af_dp = getParam("af_dp", settings.mobile_deeplink);       
+    
+        // Build URL
+        var url = baseUrl + media_source + campaign + 
+                  ad + ad_id + site_id + sub1 + af_dp;
+        
+        return url;
+    };
+
+    this.buildBanner = function(bannerContainerId, url, settings) {
+
+        // CLOSE BUTTON
+        var closeImg = document.createElement("IMG");
+        closeImg.setAttribute('class', 'appsflyer-banner-close-img');
+        closeImg.setAttribute('src', 'img/close.png');
+
+        var closeFn = "var bannerWrapper = document.getElementById('" + bannerContainerId + "'); while (bannerWrapper.firstChild) {bannerWrapper.removeChild(div.firstChild);}";
+        var closeImgContainer = document.createElement("DIV");
+        closeImgContainer.setAttribute('class', 'appsflyer-banner-close-container');
+        closeImgContainer.setAttribute('onclick', closeFn);
+        closeImgContainer.appendChild(closeImg);
+       
+        // TEXT
+        var title = document.createElement("DIV");
+        title.setAttribute('class', 'appsflyer-banner-title');
+        title.innerHTML = settings.title;
+
+        var subtitle = document.createElement("DIV");
+        subtitle.setAttribute('class', 'appsflyer-banner-subtitle');
+        subtitle.innerHTML = settings.subtitle;
+
+        var textContainer = document.createElement("DIV");
+        textContainer.setAttribute('class', 'appsflyer-banner-text-container');
+        textContainer.appendChild(title);
+        textContainer.appendChild(subtitle);
+        
+        // ICON
+        var iconImg = document.createElement("IMG");
+        iconImg.setAttribute('class', 'appsflyer-banner-icon-img');
+        iconImg.setAttribute('src', settings.app_icon);
+
+        var iconContainer = document.createElement("DIV");
+        iconContainer.setAttribute('class', 'appsflyer-banner-icon-container');
+        iconContainer.appendChild(iconImg);
+
+        // DOWNLOAD BUTTON
+        var downloadUrl = "window.location.replace('" + url + "')";
+        var downloadButton = document.createElement("BUTTON");
+        downloadButton.setAttribute('class', 'appsflyer-banner-download-button');
+        downloadButton.setAttribute('onclick', downloadUrl);
+        downloadButton.innerHTML = settings.call_to_action || CALL_TO_ACTION_TEXT;
+        
+        // BANNER CONTAINER
+        var bannerContainer = document.createElement("DIV");
+        bannerContainer.setAttribute('class', 'appsflyer-banner-container');
+
+        bannerContainer.appendChild(closeImgContainer);
+        bannerContainer.appendChild(iconContainer);
+        bannerContainer.appendChild(textContainer);
+        bannerContainer.appendChild(downloadButton);
+       
+        return bannerContainer;
+    };
+
+    this.init = function(bannerContainerId, settings) {
+        // get data
+        var url = this.buildUrl(settings);
+        var banner = this.buildBanner(bannerContainerId, url, settings);
+        
+        // inject to banner container element
+        var div = document.getElementById(bannerContainerId);
+        div.appendChild(banner);
+
+        // required for handheld devices
+        var metaTag = document.createElement('meta');
+        metaTag.name = "viewport";
+        metaTag.content = "width=device-width, initial-scale=1.0";
+        document.getElementsByTagName('head')[0].appendChild(metaTag);
+    };
+}
---
+++ readme.md
@@ -0,0 +1,116 @@
+AppsFlyer Web App Banner Generator
+==================================
+
+Web-to-App Banner Generator for converting your mobile web users into app installs. Powered by OneLink.
+
+# Table of Content
+- [Introduction](#introduction)
+- [Installation](#installation)
+    - [Required files](#required-files)
+- [Usage](#usage)
+    - [Creating your banner](#creating-your-banner)
+- [Available Parameters](#available-parameters)
+    - [Banner Settings](#banner-settings)
+    - [Attribution Parameters](#attribution-parameters)
+    - [Deep Linking Parameters](#deep-linking-parameters)
+- [License](#license)
+
+# Introduction
+The AppsFlyer Web-to-App Banner Generator allows you to easily create a mobile app promotional banner on your webpage. This banner will be displayed to your mobile web users, inviting them to download your mobile app. Using OneLink deferred deep linking, users who click on the link will be send to the right environment for their device. Users who don not have the app installed will first be sent the correct app store for their operating system. When the app is installed, OneLink opens the app to the optimal place, page or state in the app.
+
+Banners include a working, properly formatted tracking link based on your OneLink settings and a set of user-configured banner configuration.
+
+
+# Installation
+AppsFlyer recommends using the minified .js.min and .css.min versions of the banner generator.
+
+### Required files
+* appsflyer-banner.js.min
+* appsflyer-banner.css.min
+
+
+# Usage
+In your webpage HTML file, include the following inside the `<head>` element:
+```html
+<script type="text/javascript" src="appsflyer-banner.js.min"></script>
+<link rel="stylesheet" href="appsflyer-banner.css.min">
+```
+
+### Creating your banner
+The following easy steps should be followed in order to create your banner:
+
+1. Create a banner container element in your HTML file that will contain the banner, e.g.:
+```html
+<body>
+    <div id="my-banner"></div>
+    ...
+</body>
+```
+
+2. Create an AppsFlyer banner object, using:
+```js
+var banner = new AFBanner();
+```
+
+3. Create a settings JSON object, which includes the banner general settings , attribution parameters and deep link parameters (see [Available Parameters](#available-parameters)). Example settings object:
+```js
+var settings = {
+    // banner settings
+    title: "AppsFlyer",
+    subtitle: "Track campaigns on the go",
+    app_icon: "img/app_icon.png",
+    call_to_action: "Install",
+    
+    // attribution settings
+    media_source: "banner_pid",
+    campaign: "banner_c",
+    ad: "banner_ad",
+    ad_id: "banner_ad_id",
+    site_id: "banner_site_id",
+    sub1: "banner_sub1",
+    
+    // routing settings
+    onelink_id: "pGHC",
+    subdomain: "appsflyer",
+    mobile_deeplink: "appsflyer://"
+};
+```
+
+4. Initialize the banner using the `init` function, and passing both container ID and banner settings object.
+```js
+banner.init("my-banner", settings);
+``` 
+
+
+# Available Parameters
+
+### Banner Settings
+| Parameter | Type | Description |
+|:---|:---|:---|
+| title | `String` | The banner's title text |
+| subtitle | `String` | (optional) The text that will appear below the title |
+| app_icon | `String` | URL or image file location of the app that you wish to install |
+| call_to_action | `String` | The text that will be displayed in the action button, e.g. `Install` |
+
+
+### Attribution Parameters
+| Parameter | Type | Description |
+|:---|:---|:---|
+| media_source | `String` | Media Source (`pid`) |
+| campaign | `String` | Campaign |
+| ad | `String` | Ad |
+| ad_id | `String` | Ad ID |
+| site_id | `String` | Site ID |
+| sub1 | `String` | Customizable parameter (`af_sub1`) |
+
+
+### Deep Link Parameters
+| Parameter | Type | Description |
+|:---|:---|:---|
+| onelink_id | `String` | OneLink Configuration ID |
+| subdomain | `String` | Your sub-domain |
+| mobile_deeplink | `String` | Mobile deep link URL (`af_dp`) |
+
+
+# License
+MIT Licensed. Copyright (c) AppsFlyer 2018.
